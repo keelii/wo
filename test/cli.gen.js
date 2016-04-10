5d16a15929ca60af8daaae3cb30babfc2d35a977
keelii <keeliizhou@gmail.com>
2016-04-10 22:55:23 +0800

    add test cases to gen.js

--- test/cli.gen.js
+++ test/cli.gen.js
@@ -28,7 +28,7 @@ describe('cli/gen', function () {
 
     it('should throw an error while generating project', function (done) {
         gen(settings, '*()_+', function (err) {
-            assert.equal(err[0].code, 'ENOENT');
+            assert.equal(true, !!err);
             done();
         });
     });
